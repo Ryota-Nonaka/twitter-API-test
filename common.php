41f35f69672eca7e0f8e230d90c84641cae8fcc5
ノナカリョウタ <67694580+Ryota-Nonaka@users.noreply.github.com>
2020-08-15 00:13:28 +0900

    Update common.php

--- common.php
+++ common.php
@@ -4,4 +4,4 @@
 
 define('CONSUMER_KEY', 'mliv2TvmcNdH6U7XttmGCQwsW');
 define('CONSUMER_SECRET', 'qSXvkclZapRVgR2cz77nwwhpGiQLS5uiA8VtpdsOvOIAmO8k3o');
-define('OAUTH_CALLBACK', 'http://localhost/twitter%20login/calback.php');
+define('OAUTH_CALLBACK', 'http://www.example.com/callback.php');
