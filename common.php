575b2f74c8825eaf5626919e48e884c8d53f74ac
ノナカリョウタ <67694580+Ryota-Nonaka@users.noreply.github.com>
2020-08-15 00:09:07 +0900

    ソーシャルログインサンプル用
    first commit

---
+++ common.php
@@ -0,0 +1,7 @@
+<?php
+
+//common.php
+
+define('CONSUMER_KEY', 'mliv2TvmcNdH6U7XttmGCQwsW');
+define('CONSUMER_SECRET', 'qSXvkclZapRVgR2cz77nwwhpGiQLS5uiA8VtpdsOvOIAmO8k3o');
+define('OAUTH_CALLBACK', 'http://localhost/twitter%20login/calback.php');
---
+++ mypage.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+require_once 'common.php';
+require_once 'twitteroauth-main\autoload.php';
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+//セッションに入れておいたさっきの配列
+$access_token = $_SESSION['access_token'];
+
+//OAuthトークンとシークレットも使って TwitterOAuth をインスタンス化
+$connection = new TwitterOAuth('mliv2TvmcNdH6U7XttmGCQwsW', 'qSXvkclZapRVgR2cz77nwwhpGiQLS5uiA8VtpdsOvOIAmO8k3o', $access_token['oauth_token'], $access_token['oauth_token_secret']);
+
+//ユーザー情報をGET
+$user = $connection->get("account/verify_credentials");
+//(ここらへんは、Twitter の API ドキュメントをうまく使ってください)
+
+//GETしたユーザー情報をvar_dump
+var_dump($user);
